8858098d6de97cfbe03b5ac0aed50a5b5652f783
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-02-23 19:01:47 +0200

    Add files via upload

---
+++ postgres_best/Postgres230221/PostgresDAO.cs
@@ -0,0 +1,61 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgres230221
+{
+    internal class PostgresDAO : IDataAccess
+    {
+        private static readonly log4net.ILog my_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private string m_conn;
+
+        public PostgresDAO(string m_conn)
+        {
+            this.m_conn = m_conn;
+        }
+        /*
+    private static List<Movie> PrintAllMovies(string conn_string)
+        {
+            using (var conn = new NpgsqlConnection(conn_string))
+            {
+                conn.Open();
+                string query = "SELECT * FROM movies";
+
+                NpgsqlCommand command = new NpgsqlCommand(query, conn);
+                command.CommandType = System.Data.CommandType.Text; // this is default
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    long id = (long)reader["id"];
+                    string title = (string)reader["title"];
+                    // ....
+                    Console.WriteLine($"{id} {title}");
+                }
+            }
+        }
+        */
+
+        public bool TestDbConnection()
+        {
+            my_logger.Debug("Testing db access");
+            try
+            {
+                using (var my_conn = new NpgsqlConnection(m_conn))
+                {
+                    my_conn.Open();
+                    my_logger.Debug("Testing db access. succeed!");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                my_logger.Fatal($"Testing db access. Failed!. Error: {ex}");
+                return false;
+            }
+        }
+    }
+}
--- postgres_best/Postgres230221/Program.cs
+++ postgres_best/Postgres230221/Program.cs
@@ -9,32 +9,29 @@ namespace Postgres230221
 {
     class Program
     {
-        static bool TestDbConnection(string conn)
-        {
-            try
-            {
-                using (var my_conn = new NpgsqlConnection(conn))
-                {
-                    my_conn.Open();
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
+        private static readonly log4net.ILog my_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
         static void Main(string[] args)
         {
-            string conn_string = "Host=localhost;Username=postgres;Password=admin1;Database=postgres;";
-            if (TestDbConnection(conn_string))
+
+            my_logger.Info("**************************** System started");
+
+            string conn_string = "Host=localhost;Username=postgres;Password=admin;Database=postgres;";
+            IDataAccess dao = new PostgresDAO(conn_string);
+
+            if (dao.TestDbConnection())
             {
                 Console.WriteLine("Successfully connected to the DB...");
+
+                // Here
+
             }
             else
             {
                 Console.WriteLine("Failed to connected to the DB...");
             }
+
+            my_logger.Info("**************************** System shutdown");
         }
     }
 }
