d88b856057d5178d37f3dd8ae2e27be61faaea02
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-02-23 18:32:26 +0200

    Add files via upload

---
+++ postgres_best/Postgres230221/Program.cs
@@ -0,0 +1,40 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postgres230221
+{
+    class Program
+    {
+        static bool TestDbConnection(string conn)
+        {
+            try
+            {
+                using (var my_conn = new NpgsqlConnection(conn))
+                {
+                    my_conn.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string conn_string = "Host=localhost;Username=postgres;Password=admin1;Database=postgres;";
+            if (TestDbConnection(conn_string))
+            {
+                Console.WriteLine("Successfully connected to the DB...");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connected to the DB...");
+            }
+        }
+    }
+}
